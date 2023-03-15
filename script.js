efaf0001113e706394708663101d8c70570d69ee
Megalord12 <73579182+Megalord12@users.noreply.github.com>
2023-03-15 10:17:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-celebrate(){
-  if(!document.getElementById('img'))
-  {var image=document.createElement('img');
-  image.id="img";
-  image.src="https://pozdravoks.ru/_ph/15/2/596182826.gif";
-  document.body.appendChild(image)}
-}
+celebrate(){if(!document.getElementById('img')){var image=document.createElement('img');image.id="img";image.src="https://dummyimage.com/100x100/b8b8b8/000000.png";document.body.appendChild(image)}}
