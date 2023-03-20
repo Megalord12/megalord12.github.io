4058b3b2d79b4bcce988664f66fc8441af79f8b1
Megalord12 <73579182+Megalord12@users.noreply.github.com>
2023-03-20 16:00:47 +0300

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-celebrate(){if(!document.getElementById('img')){var image=document.createElement('img');image.id="img";image.src="https://dummyimage.com/100x100/b8b8b8/000000.png";document.body.appendChild(image)}}
